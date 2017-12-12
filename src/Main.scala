f12d251eed2a13a1b84109ee8f303d8d3ed89d91
akihiro0710 <acedia2074@gmail.com>
2017-12-12 09:25:59 +0900

    Implement MyPoint #1

--- src/Main.scala
+++ src/Main.scala
@@ -2,7 +2,7 @@
   * Created by ta on 2017/11/28.
   */
 object Main {
-  def main(args: Array[String]): Unit ={
-    println("Hello, World")
+  def main(args: Array[String]): Unit = {
+    println(new MyPoint(3, 4).getR())
   }
 }
---
+++ src/MyPoint.scala
@@ -0,0 +1,15 @@
+/**
+  * Created by ta on 2017/12/03.
+  */
+class MyPoint(private val x: Int, private val y: Int) {
+  def getX() = x
+
+  def getY() = y
+
+  def getR(): Double = math.sqrt(List(getX(), getY()).map(i => math.pow(i, 2)).sum)
+
+  def getTheta(): Double = {
+    val theta = math.atan2(getX(), getY())
+    math.toDegrees(theta)
+  }
+}

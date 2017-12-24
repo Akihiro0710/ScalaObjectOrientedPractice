d98ce88593c9679163fcbfc947695570a84f54f0
akihiro0710 <acedia2074@gmail.com>
2017-12-24 22:18:43 +0900

    MyTriangleのコンストラクタを改良

--- src/MyTriangle.scala
+++ src/MyTriangle.scala
@@ -3,11 +3,11 @@
 /**
   * Created by ta on 2017/12/24.
   */
-class MyTriangle(x0: Int, y0: Int, x1: Int, y1: Int, x2: Int, y2: Int) {
-  private val points = new Array[MyPoint](3)
-  points(0) = new MyPoint(x0, y0)
-  points(1) = new MyPoint(x1, y1)
-  points(2) = new MyPoint(x2, y2)
+class MyTriangle(point0: MyPoint, point1: MyPoint, point2: MyPoint) {
+  private val points = Array(point0, point1, point2)
+
+  def this(x0: Int, y0: Int, x1: Int, y1: Int, x2: Int, y2: Int) =
+    this(new MyPoint(x0, y0), new MyPoint(x1, y1), new MyPoint(x2, y2))
 
   def getX(num: Int) = points(num).getX()
 
